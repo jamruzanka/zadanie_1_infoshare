1dc6a05805013ec348ae0e18ec29c540d7f65838
jamruzanka <m.w.jamrog@gmail.com>
2019-02-12 14:38:22 +0100

    cleaned up the script, more visible now

--- zadanie_1.py
+++ zadanie_1.py
@@ -2,8 +2,7 @@ import numpy as np
 file = np.loadtxt('rows.txt')
 diff_sum = 0
 for line in file:
-    print("Max:",int(max(line)),"Min:",int(min(line)))
     diff = int(max(line)) - int(min(line))
-    print("Difference:",diff)
+    print("Max:",int(max(line)),"Min:",int(min(line)),"Difference:",diff)
     diff_sum = diff_sum + diff
-print("Sum of all differences:",diff_sum)
+print("\nThe sum of all differences:",diff_sum)
